a5bea37923ddc5e3b510a95208b6597a9d5858e0
Jakelayman0110 <99349809+Jakelayman0110@users.noreply.github.com>
2023-11-08 22:52:13 -0600

    update

--- app.js
+++ app.js
@@ -1,114 +1,41 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var Donut = require("./models/donut");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-require("dotenv").config();
-const connectionString = process.env.MONGO_CON;
-mongoose = require("mongoose");
-mongoose.connect(connectionString);
-
-//Get the default connection
-var db = mongoose.connection;
-//Bind connection to error event
-db.on("error", console.error.bind(console, "MongoDB connectionerror:"));
-db.once("open", function () {
-  console.log("Connection to DB succeeded");
-});
-
-// We can seed the collection if needed on server start
-async function recreateDB() {
-  // Delete everything
-  await Table.deleteMany();
-
-  let instance1 = new Donut({
-    cakeFlavor: "regular",
-    iceFlavor: "strawberry",
-    sprinkleAmount: 5,
-  });
-  let instance2 = new Donut({
-    cakeFlavor: "chocolate",
-    iceFlavor: "glazed",
-    sprinkleAmount: 5,
-  });
-  let instance3 = new Donut({
-    cakeFlavor: "regular",
-    iceFlavor: "chocolate",
-    sprinkleAmount: 10,
-  });
-
-  instance1
-    .save()
-    .then((doc) => {
-      console.log("First object saved");
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-  instance2
-    .save()
-    .then((doc) => {
-      console.log("Second object saved");
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-  instance3
-    .save()
-    .then((doc) => {
-      console.log("Third object saved");
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
-
-let reseed = true;
-if (reseed) {
-  recreateDB();
-}
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var donutRouter = require("./routes/table");
-var boardRouter = require("./routes/board");
-var chooseRouter = require("./routes/choose");
-var resourceRouter = require("./routes/resource");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/donut", tableRouter);
-app.use("/board", boardRouter);
-app.use("/choose", chooseRouter);
-app.use("/resource", resourceRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
