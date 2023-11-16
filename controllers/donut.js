0ef82d6cfab60553e72ce9c1e1010d59cec2376a
Jakelayman0110 <99349809+Jakelayman0110@users.noreply.github.com>
2023-11-15 22:21:16 -0600

    Update donut.js

--- controllers/donut.js
+++ controllers/donut.js
@@ -1,16 +1,16 @@
 var Donut = require("../models/donut");
 
-//List of all Tables
+//List of all Donut
 exports.donut_list = function (req, res) {
-  res.send("NOT IMPLEMENTED: Table list");
+  res.send("NOT IMPLEMENTED: Donut list");
 };
 
-//for a specific Table
+//for a specific Donut
 exports.donut_detail = function (req, res) {
   res.send("NOTIMPLEMENTED: Donut detail: " + req.params.id);
 };
 
-// Handle Table create on POST.
+// Handle Donut create on POST.
 exports.donut_create_post = async function (req, res) {
   console.log(req.body);
   let document = new Table();
@@ -29,16 +29,16 @@ exports.donut_create_post = async function (req, res) {
   }
 };
 
-// Handle Table delete form on DELETE.
+// Handle Donut delete form on DELETE.
 exports.donut_delete = function (req, res) {
   res.send("NOT IMPLEMENTED: Donut delete DELETE " + req.params.id);
 };
-// Handle Table update form on PUT.
+// Handle Donut update form on PUT.
 exports.donut_update_put = function (req, res) {
   res.send("NOT IMPLEMENTED: Donut update PUT" + req.params.id);
 };
 
-// List of all Tables
+// List of all Donuts
 exports.donut_list = async function (req, res) {
   try {
     theDonut = await Donut.find();
